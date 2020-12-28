f115e4f0cfabe61d3da03331fccb6e4585f75983
Shineslee <46121976+Shineslee@users.noreply.github.com>
2020-12-28 14:41:02 +0800

    Update jd_jdzz.js

--- jd_jdzz.js
+++ jd_jdzz.js
@@ -44,8 +44,9 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const inviteCodes = [
-  `ATGEC3-fsrn13aiaEqiM@AUWE5maSSnzFeDmH4iH0elA@ATGEC3-fsrn13aiaEqiM@AUWE5m6WUmDdZC2mr1XhJlQ@AUWE5m_jEzjJZDTKr3nwfkg@A06fNSRc4GIqY38pMBeLKQE2InZA@AUWE5mf7ExDZdDmH7j3wfkA`,
-  `ATGEC3-fsrn13aiaEqiM@AUWE5maSSnzFeDmH4iH0elA@ATGEC3-fsrn13aiaEqiM@AUWE5m6WUmDdZC2mr1XhJlQ@AUWE5m_jEzjJZDTKr3nwfkg@A06fNSRc4GIqY38pMBeLKQE2InZA`
+  `S5KkcRBoZ81WGchrykv8KdQ@S5KkcR0sa_AGBdBv3xvdfdA`,
+  `SaU7cmKSYIdxR94JJQ32PoSP5vCp8@S5KkcR0sa_AGBdBv3xvdfdA`,
+  `SaU7cmKSYIdxR94JJQ32PoSP5vCp8@S5KkcRBoZ81WGchrykv8KdQ`
 ]
 !(async () => {
   $.tuanList = []
